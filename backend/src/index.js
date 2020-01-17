8be6c42a7cdf8ab533969365cd5cd5f964217bae
diegosoriarios <diegosoriarios@gmail.com>
2020-01-17 12:37:20 -0300

    SocketIO

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
 const cors = require('cors')
+const http = require("http")
+const { setupWebSocket } = require('./websocket')
 
 const app = express()
+const server = http.Server(app)
+
+setupWebSocket(server)
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@semanaomnistack10-k2zt9.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -14,4 +19,6 @@ app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+server.listen(3333, () => {
+    console.log("Server started on port 3333")
+})
--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -12,6 +12,7 @@ import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import api from '../services/api'
+import { connect, disconnect, subscribeToNewDevs } from '../services/socket'
 
 function Main({ navigation }) {
     const [devs, setDevs] = useState([])
@@ -41,6 +42,21 @@ function Main({ navigation }) {
         loadInitialPosition()
     }, [])
 
+    useEffect(() => {
+        subscribeToNewDevs(dev => setDevs([...devs, dev]))
+    }, [devs])
+
+    function setupWebSocket() {
+        disconnect()
+
+        const { latitude, longitude } = currentRegion
+        connect(
+            latitude,
+            longitude,
+            techs
+        )
+    }
+
     async function loadDevs() {
         const { latitude, longitude } = currentRegion
 
@@ -53,6 +69,7 @@ function Main({ navigation }) {
         })
 
         setDevs(response.data.devs)
+        setupWebSocket()
     }
 
     function handleRegionChanged(region) {
